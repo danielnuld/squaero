d58c702f79465a2e19313f6421a480bfde83b4d0
Daniel Noé Núñez López <daniel.nunez@stjsonora.gob.mx>
2026-09-15 12:42:00 -0700

    site: publish 2026-09-15T19:42Z

--- cmake/libpq-win32/shims/netinet/tcp.h
+++
@@ -1,3 +0,0 @@
-/* mingw shim: TCP socket options live in winsock2/ws2tcpip. */
-#include <winsock2.h>
-#include <ws2tcpip.h>
--- drivers/informix/src/utils/clientmissing.c
+++
@@ -1,8 +0,0 @@
-#include "clientmissing.h"
-
-#include <string.h>
-
-int ifx_client_missing(int direct, const char *sqlstate)
-{
-    return !direct && sqlstate != NULL && strcmp(sqlstate, "IM002") == 0;
-}
